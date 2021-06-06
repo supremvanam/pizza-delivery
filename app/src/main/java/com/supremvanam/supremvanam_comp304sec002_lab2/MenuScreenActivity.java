128156b0d4ba38b92623cd791b2feb65f64b4268
Suprem Vanam <supremvanam.gsp@gmail.com>
2021-06-06 23:35:50 +0530

    Updated MainActivity Theme

---
+++ app/src/main/java/com/supremvanam/supremvanam_comp304sec002_lab2/MenuScreenActivity.java
@@ -0,0 +1,14 @@
+package com.supremvanam.supremvanam_comp304sec002_lab2;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MenuScreenActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_menu_screen);
+    }
+}
