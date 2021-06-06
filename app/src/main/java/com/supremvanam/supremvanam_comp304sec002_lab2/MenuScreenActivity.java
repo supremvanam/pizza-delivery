a94f17259ce845ca7ad9c31c5c5aca1f7dcb51fc
Suprem Vanam <supremvanam.gsp@gmail.com>
2021-06-07 01:03:19 +0530

    Added Menu Items in MenuScreenActivity

--- app/src/main/java/com/supremvanam/supremvanam_comp304sec002_lab2/MenuScreenActivity.java
+++ app/src/main/java/com/supremvanam/supremvanam_comp304sec002_lab2/MenuScreenActivity.java
@@ -1,8 +1,14 @@
 package com.supremvanam.supremvanam_comp304sec002_lab2;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toast;
+
+import java.util.Objects;
 
 public class MenuScreenActivity extends AppCompatActivity {
 
@@ -10,5 +16,33 @@ public class MenuScreenActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_menu_screen);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.pizza_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.canadianPizza:
+                Toast.makeText(MenuScreenActivity.this, "Canadian clicked", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.caesarPizza:
+                Toast.makeText(MenuScreenActivity.this, "Caesar clicked", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.hawaiianPizza:
+                Toast.makeText(MenuScreenActivity.this, "Hawaii clicked", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.baconPizza:
+                Toast.makeText(MenuScreenActivity.this, "Bacon clicked", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.veggiePizza:
+                Toast.makeText(MenuScreenActivity.this, "Veggie clicked", Toast.LENGTH_SHORT).show();
+                break;
+        }
+        return true;
     }
 }
